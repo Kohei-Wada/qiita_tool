da1abf3cb81c88c50959ce5bff770f31845db48b
Kohei-Wada <program3152019@gmail.com>
2021-06-25 01:00:55 +0900

    add class QiitaComment , class QiitaTag

---
+++ QiitaTag.py
@@ -0,0 +1,6 @@
+class QiitaTag:
+    def __init__(self, followers_count, icon_url, tag_id, items_count):
+        self.followers_count = followers_count
+        self.icon_url = icon_url
+        self.id = tag_id
+        self.items_count = items_count
